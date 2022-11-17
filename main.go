9bd2f883ea86d6af0692d4321df6ca74987b0f1c
George <88823233+Georgee1337@users.noreply.github.com>
2022-11-17 13:49:40 -0500

    Add files via upload

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+var (
+	listen = flag.String("listen", ":3000", "listen address")
+)
+
+func filesHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	err := r.ParseMultipartForm(200000)
+	if err != nil {
+		log.Printf("Err: %v", err)
+	}
+
+	file, handler, err := r.FormFile("file")
+	if err != nil {
+		log.Printf("Err: %v", err)
+		return
+	}
+	defer file.Close()
+
+	resFile, err := os.Create("./data/" + handler.Filename)
+	if err != nil {
+		log.Printf("Err: %v", err)
+	}
+	defer resFile.Close()
+	if err == nil {
+		io.Copy(resFile, file)
+		defer resFile.Close()
+		log.Printf("Successfully uploaded file %q...", handler.Filename)
+	}
+
+}
+
+func main() {
+	flag.Parse()
+	log.Printf("Starting server at port %q...", *listen)
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/files/upload", filesHandler)
+
+	http.ListenAndServe(*listen, nil)
+
+}
